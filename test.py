c65f6341a364e2dc407e8c7e627664f94f2f7d1f
stiv <stivenmetaj@gmail.com>
2019-01-23 13:41:57 +0100

    aggiunta di commenti vari e revisione codice 3

--- test.py
+++ test.py
@@ -10,18 +10,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-#Ritorna la proiezione orizzontale. In input si ha l'immagine target
+
+# Ritorna la proiezione orizzontale. In input si ha l'immagine target
 def getHorizontalProjection(img):
     return np.sum(img, axis=1)
 
-#Ritorna il numero di linee di una immagine di Staff. In input si ha l'immagine target
+
+# Ritorna il numero di linee di una immagine di Staff. In input si ha l'immagine target
 def getNumLines(imgStaff):
     horProj = getHorizontalProjection(imgStaff)
     pentasSeparators = getPentasSeparators(horProj)
     return (len(pentasSeparators) - 1) * 5
 
-#Ritorna le posizioni dei separatori dei pentagrammi
-#In input si ha la proiezione orizzontale di una immagine
+
+# Ritorna le posizioni dei separatori dei pentagrammi
+# In input si ha la proiezione orizzontale di una immagine
 def getPentasSeparators(horizontalProjection):
     th = np.max(horizontalProjection) / 2
     peak = False
@@ -51,8 +54,9 @@ def getPentasSeparators(horizontalProjection):
     cuts.append(int((y + previousEnd) / 2))
     return cuts
 
-#Ritorna una lista di x tuple per x pentagrammi. Ogni tupla contiene posizione di inizio e di fine del pentagramma
-#In input si ha la proiezione orizzontale di una immagine
+
+# Ritorna una lista di x tuple per x pentagrammi. Ogni tupla contiene posizione di inizio e di fine del pentagramma
+# In input si ha la proiezione orizzontale di una immagine
 def getPentasLimits(horizontalProjection):
     th = np.max(horizontalProjection) / 2
     peak = False
@@ -80,16 +84,16 @@ def getPentasLimits(horizontalProjection):
 
     return limits
 
-#Ritorna l'immagine (deepcopy) con l'aggiunta di quadrati a seconda del clsname che si cerca
-#In input si ha l'immagine, il clsname da trovare e il documento XML di supervisione
+
+# Ritorna l'immagine (deepcopy) con l'aggiunta di quadrati a seconda del clsname che si cerca
+# In input si ha l'immagine, il clsname da trovare e il documento XML di supervisione
 def squarize(doc, img, clsnameBeginning=""):
     tmpImg = copy.deepcopy(img)
 
     for k in range(len(doc)):
-        #scarta gli elementi nell'xml che non si vogliono
+        # scarta gli elementi nell'xml che non si vogliono
         if not doc[k].clsname.startswith(clsnameBeginning):
             continue
-
 
         l = doc[k].left
         t = doc[k].top
@@ -99,35 +103,36 @@ def squarize(doc, img, clsnameBeginning=""):
         squaresColor = 0.8
 
         for i in range(w):
-            tmpImg[t][l+i] = squaresColor
-            tmpImg[t+h][l+i] = squaresColor
+            tmpImg[t][l + i] = squaresColor
+            tmpImg[t + h][l + i] = squaresColor
 
         for i in range(h):
-            tmpImg[t+i][l] = squaresColor
-            tmpImg[t+i][l+w] = squaresColor
+            tmpImg[t + i][l] = squaresColor
+            tmpImg[t + i][l + w] = squaresColor
 
     return tmpImg
 
-#Ritorna una lista di liste. Ogni sottolista contiene le note di un pentagramma
-#in ordine del parametro left del XML
-#In input si ha il documento XML di supervisione e la lista
+
+# Ritorna una lista di liste. Ogni sottolista contiene le note di un pentagramma
+# in ordine del parametro left del XML
+# In input si ha il documento XML di supervisione e la lista
 def getOrderedNotesAnnotations(doc, pentasSeparators):
     notes = []
     for i in range(len(pentasSeparators) - 1):
         notes.append([])
 
     for k in range(len(doc)):
-        #scarto tutto quello che non è una nota
+        # scarto tutto quello che non è una nota
         if not doc[k].clsname.startswith("notehead"):
             continue
 
         # guardo in quale lista devo appendere la nota
-        yNoteheadCenter = doc[k].top + (doc[k].height/2)
+        yNoteheadCenter = doc[k].top + (doc[k].height / 2)
         i = 0
         while yNoteheadCenter > pentasSeparators[i]:
             i += 1
         # appendo la nota alla lista giusta
-        notes[i-1].append(doc[k])
+        notes[i - 1].append(doc[k])
 
     # ordino le liste in base al "left"
     for i in range(len(notes)):
@@ -135,8 +140,9 @@ def getOrderedNotesAnnotations(doc, pentasSeparators):
 
     return notes
 
-#Ritorna l'immagine (deepcopy) con l'aggiunta dei pixel per avere staff continue
-#In input si ha l'immagine delle staff
+
+# Ritorna l'immagine (deepcopy) con l'aggiunta dei pixel per avere staff continue
+# In input si ha l'immagine delle staff
 def getPreprocessedStaffImage(imgStaff):
     img = copy.deepcopy(imgStaff)
     numLines = getNumLines(imgStaff)
@@ -145,109 +151,122 @@ def getPreprocessedStaffImage(imgStaff):
     for x in range(img.shape[1]):
         for y in range(len(img)):
             flag = True
-            #controllo di non contare linee già processate
+            # controllo di non contare linee già processate
             for ey in yExploredLines:
-                #le posizioni salvate vanno controllate con un errore (di 4 in questo caso)
+                # le posizioni salvate vanno controllate con un errore (di 4 in questo caso)
                 if abs(ey - y) < 4:
                     flag = False
                     break
 
-            #se trovo un pixel allora inizio a processare la linea
+            # se trovo un pixel allora inizio a processare la linea
             if img[y][x] == 1.0 and flag:
                 numLines -= 1
-                #aggiungo la posizione della linea a quelle esplorate
+                # aggiungo la posizione della linea a quelle esplorate
                 yExploredLines.append(y)
                 yCur = y
-                #scorro la linea e se non trovo pixel li aggiungo
-                for k in range(x+1, img.shape[1]):
+                # scorro la linea e se non trovo pixel li aggiungo
+                for k in range(x + 1, img.shape[1]):
                     if img[yCur][k] == 1.0:
                         continue
-                    elif img[yCur-1][k] == 1.0:
+                    elif img[yCur - 1][k] == 1.0:
                         yCur -= 1
-                    elif img[yCur+1][k] == 1.0:
+                    elif img[yCur + 1][k] == 1.0:
                         yCur += 1
                     else:
                         img[yCur][k] = 1.0
 
-                #se non ho più linee ritorno l'immagine
+                # se non ho più linee ritorno l'immagine
                 if numLines == 0:
                     return img
     return "Error: not enough lines found"
 
-#Ritorna l'immagine (deepcopy) con l'aggiunta dei ledgers (linee per note esterne)
-#In input si ha l'immagine delle staff e il documento XML di supervisione
+
+# Ritorna l'immagine (deepcopy) con l'aggiunta dei ledgers (linee per note esterne)
+# In input si ha l'immagine delle staff e il documento XML di supervisione
 def getStaffImageWithLedgers(imgStaff, doc):
     img = copy.deepcopy(imgStaff)
 
     for annotation in doc:
-        #voglio solo i ledgers
+        # voglio solo i ledgers
         if not annotation.clsname.startswith("ledge"):
             continue
-        y = annotation.top + int(annotation.height/2)
+        y = annotation.top + int(annotation.height / 2)
         for x in range(annotation.width):
-            #disegno il ledger
+            # disegno il ledger
             img[y][annotation.left + x] = 1.0
     return img
 
-#Ritorna l'immagine (deepcopy) con l'aggiunta dei punti centrali delle note
-#In input si ha l'immagine delle staff e il documento XML di supervisione
+
+# Ritorna l'immagine (deepcopy) con l'aggiunta dei punti centrali delle note
+# In input si ha l'immagine delle staff e il documento XML di supervisione
 def addNotesCenter(imgStaff, doc):
     img = copy.deepcopy(imgStaff)
 
     for annotation in doc:
         if not annotation.clsname.startswith("notehead"):
             continue
-        y = annotation.top + int(annotation.height/2)
-        x = annotation.left + int(annotation.width/2)
+        y = annotation.top + int(annotation.height / 2)
+        x = annotation.left + int(annotation.width / 2)
         img[y][x] = 0.7
 
     return img
 
-#Ritorna la lista delle posizioni all'interno del pentagramma (up e low) per ogni nota
-#In input l'immagine delle staff, la lista delle note
+
+# Ritorna la lista delle posizioni all'interno del pentagramma (up e low) per ogni nota
+# In input si hanno l'immagine delle staff e la lista delle note
 def getNotesPentasPositions(imgStaff, imgStaffLedgers, notesAnnotations):
     notesPositions = []
-    limits = getPentasLimits(getHorizontalProjection(imgStaff))
-    #solo le note il cui centro è tra limit0-stop e limit1+stop vengono prese in considerazione
-    stopValue = ((limits[0][1] - limits[0][0])/4) * 1.5
+    pentasLimits = getPentasLimits(getHorizontalProjection(imgStaff))
+    # lo stopValue indica la distanza tra una riga e l'altra, con un eccesso del 50% (*1,5)
+    stopValue = ((pentasLimits[0][1] - pentasLimits[0][0]) / 4) * 1.5
 
-    for annotatedStaff in notesAnnotations:
-        #per ogni pentagramma avro' una lista notesPos
+    for staff in notesAnnotations:
+        # per ogni pentagramma avro' una lista notesPos
         notesPos = []
-        for annotation in annotatedStaff:
-            if isInsideStaff(annotation, limits):
-                #notePos = insideStaffPosition(imgStaff, annotation, stopValue)
-                u, l = insideStaffPosition(imgStaff, annotation, stopValue)
-                if u == 5 and l == 0:
-                    pass
-            # TODO gestire fuori pentagramma
-            # else:
-            #     notesPos = ...
-            # notesPos.append(notePos)
+        for note in staff:
+            if isInsideStaff(note, pentasLimits):
+                u, l = getInsideStaffNotePosition(imgStaff, note, stopValue)
+            # TODO gestire fuori pentagramma (funzione outside vuota)
+            else:
+                u, l = outsideStaffPosition(imgStaff, note, stopValue)
+            notesPos.append((u, l))
         notesPositions.append(notesPos)
 
     return notesPositions
 
-def isInsideStaff(annotation, limits):
-    # TODO cambiare/aggiungere implementazione (guardando XML)
-    yAnnotationCenter = annotation.top + int(annotation.height/2)
-    eps = int(annotation.height*3/4)
 
-    for limit in limits:
+# Ritorna True se la nota si trova all'interno di uno degli intervalli di limits
+# In input si hanno la nota (noteAnnotation) e i limiti del pentagramma
+def isInsideStaff(noteAnnotation, pentalimits):
+    # TODO cambiare/aggiungere implementazione (guardando XML)
+    yAnnotationCenter = noteAnnotation.top + int(noteAnnotation.height / 2)
+    # con eps vado a prendere anche le note che non si trovano in modo preciso tra i limits
+    # ma che comunque devono essere considerate "Inside Notes"
+    eps = int(noteAnnotation.height * 3 / 4)
+
+    for limit in pentalimits:
         if limit[0] - eps < yAnnotationCenter < limit[1] + eps:
             return True
 
     return False
 
-def insideStaffPosition(imgStaff, annotation, stopValue):
 
-    yAnnotationCenter = annotation.top + int(annotation.height/2)
-    xAnnotationCenter = annotation.left + int(annotation.width/2)
+# Ritorna la posizioe della nota all'interno del pentagramma
+# in uscita si hanno 2 interi, U ed L (up e low) che indicano il numero di righe
+# sopra e sotto la nota
+#
+# Se la somma dei numeri e' 5 la nota si trova tra due linee, altrimenti (somma=4) su una linea
+# In input si hanno l'immagine delle staff, la nota e il valore per cui smetto di contare le righe
+def getInsideStaffNotePosition(imgStaff, noteAnnotation, stopValue):
+    yAnnotationCenter, xAnnotationCenter = getNoteCoordinates(noteAnnotation)
 
+    #TODO controllare se la funzione getNoteNumeLines funziona
+    #upperLines = getNoteNumLines(imgStaff, noteAnnotation, stopValue, "U")
     numBlacks = 0
     upperLines = 0
     lastBlack = True
-    i = int(annotation.height/3)
+    i = int(noteAnnotation.height / 3)
+    # cerco le upperLines
     while numBlacks < stopValue:
         if imgStaff[yAnnotationCenter - i][xAnnotationCenter] == 0.0:
             numBlacks += 1
@@ -259,10 +278,12 @@ def insideStaffPosition(imgStaff, annotation, stopValue):
             numBlacks = 0
         i += 1
 
+    #lowerLines = getNoteNumLines(imgStaff, noteAnnotation, stopValue, "U")
     numBlacks = 0
     lowerLines = 0
     lastBlack = True
-    i = int(annotation.height/3)
+    i = int(noteAnnotation.height / 3)
+    # cerco le lowerLines
     while numBlacks < stopValue:
         if imgStaff[yAnnotationCenter + i][xAnnotationCenter] == 0.0:
             numBlacks += 1
@@ -276,8 +297,39 @@ def insideStaffPosition(imgStaff, annotation, stopValue):
 
     return upperLines, lowerLines
 
+#Ritorna le coordinate y e x della nota
+def getNoteCoordinates(noteAnnotation):
+    return noteAnnotation.top + int(noteAnnotation.height / 2), noteAnnotation.left + int(noteAnnotation.width / 2)
+
+
+def getNoteNumLines(imgStaff, noteAnnotation, stopValue, isUpOrLow):
+    yAnnotationCenter, xAnnotationCenter = getNoteCoordinates(noteAnnotation)
+
+    numBlacks = 0
+    lines = 0
+    lastBlack = True
+    i = int(noteAnnotation.height / 3)
+    # cerco le lines
+    while numBlacks < stopValue:
+        if isUpOrLow == "U":
+            if imgStaff[yAnnotationCenter - i][xAnnotationCenter] == 0.0:
+                numBlacks += 1
+                lastBlack = True
+        elif isUpOrLow == "L":
+            if imgStaff[yAnnotationCenter + i][xAnnotationCenter] == 0.0:
+                numBlacks += 1
+                lastBlack = True
+        elif lastBlack:
+            # salita
+            lastBlack = False
+            lines += 1
+            numBlacks = 0
+        i += 1
+
+
 def outsideStaffPosition(imgStaff, annotation, stopValue):
-    pass
+    return 1, 1
+
 
 def getStaffsFromImage(img):
     horizontalProjection = np.sum(img, axis=1)
@@ -285,13 +337,15 @@ def getStaffsFromImage(img):
     staffs = []
 
     for i in range(len(pentasSeparators) - 1):
-        staff = img[pentasSeparators[i] : pentasSeparators[i+1]]
+        staff = img[pentasSeparators[i]: pentasSeparators[i + 1]]
         staffs.append(staff)
 
     return staffs
 
+
 def resizePatch(patch, w, h):
     return cv2.resize(patch, dsize=(w, h), interpolation=cv2.INTER_AREA)
+
 
 def getPatchesFromStaff(staff, w, h):
     patches = []
@@ -300,17 +354,17 @@ def getPatchesFromStaff(staff, w, h):
     transpStaff = staff.transpose()
     n = int(staff.shape[1] / l)
     for i in range(n):
-        patch = transpStaff[i*l : (i+1)*l].transpose()
+        patch = transpStaff[i * l: (i + 1) * l].transpose()
         patch = resizePatch(patch, w, h)
         patches.append(patch)
 
-        iRand = random.randint(0, staff.shape[1]-l)
-        patch = transpStaff[iRand : iRand+l].transpose()
+        iRand = random.randint(0, staff.shape[1] - l)
+        patch = transpStaff[iRand: iRand + l].transpose()
         patch = resizePatch(patch, w, h)
         patches.append(patch)
-
 
     return patches
+
 
 def ciclone():
     # TODO capire differenza tra i due XML
@@ -347,13 +401,11 @@ def ciclone():
         # plt.plot(sums.tolist())
         # plt.show()
 
-
         # imgStaff = getPreprocessedStaffImage(imgStaff)
         # imgStaffLedgers = addLedgers(imgStaff, doc)
         #
         # plt.imshow(imgStaff, cmap="gray")
         # plt.show()
-
 
         # notesPositions = getNotesPentasPositions(imgStaff, imgStaffLedgers, notesAnnotations)
 
@@ -378,5 +430,3 @@ if __name__ == "__main__":
         for patch in patches:
             plt.imshow(patch, cmap="gray")
             plt.show()
-
-
