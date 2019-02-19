dfffeda10cbbeda7e5d10098c029c957f11d8898
fedem96 <federicomagno96@gmail.com>
2019-02-19 21:33:06 +0100

    plots for comparison of different variants

--- maskrcnn-benchmark/tools/evaluate_and_plot.py
+++ maskrcnn-benchmark/tools/evaluate_and_plot.py
@@ -30,8 +30,8 @@ from bisect import bisect_left
 from tqdm import tqdm
 
 # Use parameter DATASETS.TEST to change evaluated datasets
-# Example: DATASETS.TEST ('muscima_train', 'muscima_val')
-# Note: value specified must be a tuple and single quotes must be used
+# Example: DATASETS.TEST "('muscima_train', 'muscima_val')"
+# Note: value specified must be a tuple
 
 # Use parameter MODEL.DEVICE to run on different device
 # Example: MODEL.DEVICE "cpu"
@@ -46,7 +46,7 @@ def main():
         help="path to config file",
     )
     parser.add_argument("--local_rank", type=int, default=0)
-    parser.add_argument("--gpu", type=int, default=0, help="gpu to use")
+    parser.add_argument("--output_dirs", type=str, default=None, help="multiple dirs, will be compared")
     parser.add_argument(
         "opts",
         help="Modify config options using the command-line",
@@ -69,17 +69,22 @@ def main():
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
 
-    if args.config_file.startswith("configs/"):
-        # se il file di configurazione specificato sta nella cartella configs, allora determino automaticamente la cartella di output
-        pthDir = "models/" + args.config_file[len("configs/"):-len(".yaml")]
-        cfg.update({"OUTPUT_DIR": pthDir})
+    if args.output_dirs is None:
+        if args.config_file.startswith("configs/"):
+            # se il file di configurazione specificato sta nella cartella configs, allora determino automaticamente la cartella di output
+            variant_folders = ("models/" + args.config_file[len("configs/"):-len(".yaml")],)
+            cfg.update({"OUTPUT_DIR": variant_folders[0]})
+        else:
+            # se il file di configurazione non sta nella cartella configs, allora l'output deve essere specificato mediante OUTPUT_DIR
+            variant_folders = (cfg.OUTPUT_DIR,)
     else:
-        # se il file di configurazione non sta nella cartella configs, allora l'output deve essere specificato mediante il parametro OUTPUT_DIR
-        pthDir = cfg["OUTPUT_DIR"]
+        # se ho specificato --output_dirs, questo parametro ha maggiore priorità rispetto ad OUTPUT_DIR e al --config-file
+        # --output_dirs serve per confrontare varianti diverse
+        variant_folders = args.output_dirs.split(",")
 
     cfg.freeze()
 
-    save_dir = pthDir
+    save_dir = ""
     logger = setup_logger("maskrcnn_benchmark", save_dir, get_rank())
     logger.info("Using {} GPUs".format(num_gpus))
     logger.info(cfg)
@@ -87,81 +92,95 @@ def main():
     logger.info("Collecting env info (might take some time)")
     logger.info("\n" + collect_env_info())
 
-    pthFiles = [f for f in os.listdir(pthDir) if os.path.isfile(os.path.join(pthDir, f)) and f.endswith(".pth")]
-    assert all(pthFile.endswith(".pth") for pthFile in pthFiles)
-    metrics = {}
-    for pthFile in pthFiles:
-        pthPrefix = pthFile[:-4]
-        cfg['MODEL'].update({"WEIGHT": os.path.join(pthDir, pthFile)})
-        model = build_detection_model(cfg)
-        model.to(cfg.MODEL.DEVICE)
+    metrics_comparison_datasets = {}
+    metrics_comparison_variants = {}
 
-        output_dir = cfg.OUTPUT_DIR
-        checkpointer = DetectronCheckpointer(cfg, model, save_dir=output_dir)
-        _ = checkpointer.load(cfg.MODEL.WEIGHT)
+    for jdx, variant_folder in enumerate(variant_folders):
 
-        iou_types = ("bbox",)
-        if cfg.MODEL.MASK_ON:
-            iou_types = iou_types + ("segm",)
-        output_folders = [None] * len(cfg.DATASETS.TEST)
-        dataset_names = cfg.DATASETS.TEST
-        if cfg.OUTPUT_DIR:
-            for idx, dataset_name in enumerate(dataset_names):
-                # TODO per cambiare cartella salvataggio metriche, cambiare qui cfg.OUTPUT_DIR con altro percorso
-                output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name, pthPrefix)
-                mkdir(output_folder)
-                output_folders[idx] = output_folder
-        data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
-        for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
-            if not os.path.exists(os.path.join(output_folder, "coco_results.pth")):
-                inference(
-                    model,
-                    data_loader_val,
-                    dataset_name=dataset_name,
-                    iou_types=iou_types,
-                    box_only=cfg.MODEL.RPN_ONLY,
-                    device=cfg.MODEL.DEVICE,
-                    expected_results=cfg.TEST.EXPECTED_RESULTS,
-                    expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
-                    output_folder=output_folder,
-                )
-            synchronize()
+        cfg['OUTPUT_DIR'] = variant_folder
+        pthFiles = [f for f in os.listdir(variant_folder) if os.path.isfile(os.path.join(variant_folder, f)) and f.endswith(".pth")]
+        assert all(pthFile.endswith(".pth") for pthFile in pthFiles)
 
-        for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
+        for pthFile in pthFiles:
+            pthPrefix = pthFile[:-4]
+            cfg['MODEL'].update({"WEIGHT": os.path.join(variant_folder, pthFile)})
+            model = build_detection_model(cfg)
+            model.to(cfg.MODEL.DEVICE)
 
-            avgDistFilePath = os.path.join(output_folder, "average_distance.pth")
-            if not os.path.exists(avgDistFilePath):
-                AD = 0
-                truths = loadPredictionsFromJsonData(data_loader_val.dataset.coco.dataset['annotations'])
-                preds = loadPredictionsFromJson(os.path.join(output_folder, "bbox.json"), 0.7)
+            output_dir = cfg.OUTPUT_DIR
+            checkpointer = DetectronCheckpointer(cfg, model, save_dir=output_dir)
+            _ = checkpointer.load(cfg.MODEL.WEIGHT)
 
-                allkeys = set(preds.keys()).union(set(truths.keys()))
-                for image in allkeys:
-                    if image not in truths or image not in preds:
-                        AD += 1
-                        continue
-                    trueSeq = getLabelsSequence(*sortAnnotations(truths[image]["bboxes"], truths[image]["labels"]))
-                    predSeq = getLabelsSequence(*sortAnnotations(preds[image]["bboxes"], preds[image]["labels"]))
-                    AD += normalizedSequencesDistance(trueSeq, predSeq)
-                AD /= len(allkeys)
-                torch.save(AD, avgDistFilePath)
+            iou_types = ("bbox",)
+            if cfg.MODEL.MASK_ON:
+                iou_types = iou_types + ("segm",)
+            output_folders = [None] * len(cfg.DATASETS.TEST)
+            dataset_names = cfg.DATASETS.TEST
+            if cfg.OUTPUT_DIR:
+                for idx, dataset_name in enumerate(dataset_names):
+                    output_folder = os.path.join(cfg.OUTPUT_DIR, "inference", dataset_name, pthPrefix)
+                    mkdir(output_folder)
+                    output_folders[idx] = output_folder
+            data_loaders_val = make_data_loader(cfg, is_train=False, is_distributed=distributed)
+            for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
+                if not os.path.exists(os.path.join(output_folder, "coco_results.pth")):
+                    inference(
+                        model,
+                        data_loader_val,
+                        dataset_name=dataset_name,
+                        iou_types=iou_types,
+                        box_only=cfg.MODEL.RPN_ONLY,
+                        device=cfg.MODEL.DEVICE,
+                        expected_results=cfg.TEST.EXPECTED_RESULTS,
+                        expected_results_sigma_tol=cfg.TEST.EXPECTED_RESULTS_SIGMA_TOL,
+                        output_folder=output_folder,
+                    )
+                synchronize()
 
-            AD = torch.load(avgDistFilePath)
-            results = torch.load(os.path.join(output_folder, "coco_results.pth")).results['bbox']
-            results["AD"] = AD
-            for metric in results:
-                if metric not in metrics:
-                    metrics[metric] = {}
-                if dataset_name not in metrics[metric]:
-                    metrics[metric][dataset_name] = {}
-                metrics[metric][dataset_name][pthPrefix] = results[metric]
+            for output_folder, dataset_name, data_loader_val in zip(output_folders, dataset_names, data_loaders_val):
 
-    plot(metrics, os.path.join(cfg.OUTPUT_DIR, "inference"))
+                avgDistFilePath = os.path.join(output_folder, "average_distance.pth")
+                if not os.path.exists(avgDistFilePath):
+                    AD = 0
+                    truths = loadPredictionsFromJsonData(data_loader_val.dataset.coco.dataset['annotations'])
+                    preds = loadPredictionsFromJson(os.path.join(output_folder, "bbox.json"), 0.7)
+
+                    allkeys = set(preds.keys()).union(set(truths.keys()))
+                    for image in allkeys:
+                        if image not in truths or image not in preds:
+                            AD += 1
+                            continue
+                        trueSeq = getLabelsSequence(*sortAnnotations(truths[image]["bboxes"], truths[image]["labels"]))
+                        predSeq = getLabelsSequence(*sortAnnotations(preds[image]["bboxes"], preds[image]["labels"]))
+                        AD += normalizedSequencesDistance(trueSeq, predSeq)
+                    AD /= len(allkeys)
+                    torch.save(AD, avgDistFilePath)
+
+                AD = torch.load(avgDistFilePath)
+                results = torch.load(os.path.join(output_folder, "coco_results.pth")).results['bbox']
+                results["AD"] = AD
+                for metric in results:
+                    if metric not in metrics_comparison_datasets:
+                        metrics_comparison_datasets[metric] = {}
+                    if dataset_name not in metrics_comparison_datasets[metric]:
+                        metrics_comparison_datasets[metric][dataset_name] = {}
+                    metrics_comparison_datasets[metric][dataset_name][pthPrefix] = results[metric]
+
+                    if dataset_name not in metrics_comparison_variants:
+                        metrics_comparison_variants[dataset_name] = {}
+                    if metric not in metrics_comparison_variants[dataset_name]:
+                        metrics_comparison_variants[dataset_name][metric] = {}
+                    if variant_folder not in metrics_comparison_variants[dataset_name][metric]:
+                        metrics_comparison_variants[dataset_name][metric][variant_folder] = {}
+                    metrics_comparison_variants[dataset_name][metric][variant_folder][pthPrefix] = results[metric]
+
+        plot_comparison_datasets(metrics_comparison_datasets, os.path.join(cfg.OUTPUT_DIR, "inference"))
+
+    plot_comparison_variants(metrics_comparison_variants, "inference")
 
 
 # crea grafici e li salva in vari file .png
-def plot(metrics, path):
-    # TODO confronto varianti diverse (per ora è solo confronto dataset diversi)
+def plot_comparison_datasets(metrics, path):
     # per ogni metrica faccio un grafico
     for metric in metrics:
         plt.title(metric)
@@ -179,9 +198,44 @@ def plot(metrics, path):
 
             plt.plot(pointsX, pointsY, label=dataset_name)
 
-        plt.legend(loc='lower right')
-        plt.savefig(os.path.join(path, metric + ".png"))
+        plt.legend(loc='best')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        imgFile = os.path.join(path, metric + ".png")
+        plt.savefig(imgFile)
         plt.clf()
+        print("plot comparison of datasets saved to: " + imgFile)
+
+
+def plot_comparison_variants(metrics, path):
+    # per ogni dataset
+    for dataset_name in metrics:
+        # per ogni metrica faccio un grafico
+        for metric in metrics[dataset_name]:
+            plt.title(metric)
+            # per ogni variante faccio una funzione
+            for variant in metrics[dataset_name][metric]:
+                pointsX = []
+                pointsY = []
+                # per ogni file .pth ho un punto nella funzione
+                for pthPrefix in metrics[dataset_name][metric][variant]:
+                    x = int(pthPrefix[-7:])             # la x del punto è l'iterazione
+                    # inserisco il punto in modo che pointsX sia ordinato per x crescente
+                    index = bisect_left(pointsX, x)
+                    pointsX.insert(index, x)
+                    pointsY.insert(index, metrics[dataset_name][metric][variant][pthPrefix])
+
+                variantLabel = variant[variant.rindex("/")+1:]
+                plt.plot(pointsX, pointsY, label=variantLabel)
+
+            plt.legend(loc='best')
+            imgDir = os.path.join(path, dataset_name)
+            if not os.path.exists(imgDir):
+                os.makedirs(imgDir)
+            imgFile = os.path.join(imgDir, metric + ".png")
+            plt.savefig(imgFile)
+            plt.clf()
+            print("plot comparison of variants saved to: " + imgFile)
 
 
 # ordina le annotazioni per xmin crescente
